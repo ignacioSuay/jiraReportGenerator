9906877c749eb6bb761709d996c0be8a233384b1
Ignacio Suay <ignacio.suay@gmail.com>
2016-01-13 16:04:11 +0000

    Create IssueDTO to load items form xml to java

---
+++ src/main/java/org/wwarn/jira/report/IssueDTO.java
@@ -0,0 +1,21 @@
+package org.wwarn.jira.report;
+
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by suay on 1/13/16.
+ */
+public class IssueDTO {
+
+
+}
