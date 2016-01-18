66ded862683de64fd1af0e1ce6cbe7871fcc27b8
Ignacio Suay <ignacio.suay@gmail.com>
2016-01-18 16:08:12 +0000

    Populate issues from xmlElement

--- src/main/java/org/wwarn/jira/report/ReadJiraXmlService.java
+++ src/main/java/org/wwarn/jira/report/ReadJiraXmlService.java
@@ -10,6 +10,8 @@ import org.xml.sax.SAXException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,6 @@ import java.util.List;
  */
 public class ReadJiraXmlService {
 
-    public static final String ITEM = "item";
 
     public static List<Issue> jiraToIssueDTO(FileInputStream inputStream) throws IOException, SAXException {
         List<Issue> issues = new ArrayList<Issue>();
@@ -28,17 +29,39 @@ public class ReadJiraXmlService {
         Document xmlDoc = parser.getDocument();
 
         final Element root= xmlDoc.getDocumentElement();
-        NodeList items = root.getElementsByTagName(ITEM);
+        NodeList items = root.getElementsByTagName(JiraNodeNames.ITEM.getName());
 
         for (int i = 0; i < items.getLength(); i++){
-            Issue issue = new Issue();
-
             Element item = (Element)items.item(i);
-            issue.setTitle(getNodeValue(item, JiraNodeNames.TITLE.name));
-
-            issues.add(issue);
+            issues.add(XmlElementToIssue(item));
         }
         return issues;
+    }
+
+
+    private static Issue XmlElementToIssue(Element item){
+        Issue issue = new Issue();
+        issue.setTitle(getNodeValue(item, JiraNodeNames.TITLE.name).trim());
+        issue.setLink(getNodeValue(item, JiraNodeNames.LINK.name).trim());
+        issue.setProject(getNodeValue(item, JiraNodeNames.PROJECT.name).trim());
+        issue.setSummary(getNodeValue(item, JiraNodeNames.SUMMARY.name).trim());
+        issue.setType(getNodeValue(item, JiraNodeNames.TYPE.name).trim());
+        issue.setPriority(getNodeValue(item, JiraNodeNames.PRIORITY.name).trim());
+        issue.setStatus(getNodeValue(item, JiraNodeNames.STATUS.name).trim());
+        issue.setResolution(getNodeValue(item, JiraNodeNames.RESOLUTION.name).trim());
+        issue.setAssignee(getNodeValue(item, JiraNodeNames.ASSIGNEE.name).trim());
+        issue.setReporter(getNodeValue(item, JiraNodeNames.REPORTER.name).trim());
+        issue.setTimeEstimate(getNodeValue(item, JiraNodeNames.TIME_ESTIMATE.name).trim());
+
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss ");
+            issue.setCreated(formatter.parse(getNodeValue(item, JiraNodeNames.CREATED.name)));
+            issue.setUpdated(formatter.parse(getNodeValue(item, JiraNodeNames.UPDATED.name)));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return issue;
     }
 
     private static String getNodeValue(Element record, String tagName){
