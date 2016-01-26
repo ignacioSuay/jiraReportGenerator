91b9485fb2a0d87d50a7606e626da2dea8cde708
Ignacio Suay <ignacio.suay@gmail.com>
2016-01-26 16:51:25 +0000

    Run ReadJiraXmlServiceTest as spring test and autowire dependencies

--- src/test/java/org/wwarn/jira/report/ReadJiraXmlServiceTest.java
+++ src/test/java/org/wwarn/jira/report/ReadJiraXmlServiceTest.java
@@ -1,6 +1,10 @@
 package org.wwarn.jira.report;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.wwarn.jira.report.domain.Issue;
 
 import java.io.File;
@@ -14,14 +18,19 @@ import static junit.framework.TestCase.assertNotNull;
 /**
  * Created by suay on 1/15/16.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
 public class ReadJiraXmlServiceTest {
 
+
+    @Autowired
+    ReadJiraXmlService readJiraXmlService;
 
     @Test
     public void testJiraToIssueDTO() throws Exception{
         File file = new File("/home/suay/dev/jiraReportGenerator/src/test/resources/sprint6.xml");
         FileInputStream f = new FileInputStream(file);
-        List<Issue> issueList = ReadJiraXmlService.jiraToIssueDTO(f);
+        List<Issue> issueList = readJiraXmlService.jiraToIssueDTO(f);
         assertNotNull(issueList);
 
     }
@@ -30,8 +39,8 @@ public class ReadJiraXmlServiceTest {
     public void testCreateWordDocument() throws Exception {
         File file = new File("/home/suay/dev/jiraReportGenerator/src/test/resources/sprint6.xml");
         FileInputStream f = new FileInputStream(file);
-        List<Issue> issueList = ReadJiraXmlService.jiraToIssueDTO(f);
-        ReadJiraXmlService.createWordDocument(issueList);
+        List<Issue> issueList = readJiraXmlService.jiraToIssueDTO(f);
+        readJiraXmlService.createWordDocument(issueList);
     }
 
     @Test
@@ -39,8 +48,8 @@ public class ReadJiraXmlServiceTest {
         File file = new File("/home/suay/dev/jiraReportGenerator/src/test/resources/sprint6.xml");
         FileInputStream f = new FileInputStream(file);
         List<JiraNodeNames> fields = Arrays.asList(JiraNodeNames.TITLE, JiraNodeNames.ASSIGNEE, JiraNodeNames.CREATED);
-        List<Issue> issueList = ReadJiraXmlService.jiraToIssueDTO(f);
-        ReadJiraXmlService.createTableByFields(issueList, fields);
+        List<Issue> issueList = readJiraXmlService.jiraToIssueDTO(f);
+        readJiraXmlService.createTableByFields(issueList, fields);
     }
 
 
